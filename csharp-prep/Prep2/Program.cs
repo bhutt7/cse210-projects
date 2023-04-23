0d7413475490f4bc867fea5047587a27b29c0bea
Braden Huttash <hut21013@byui.edu>
2023-04-22 19:12:21 -0600

    Finished prep 2 core

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentInput = Console.ReadLine();
+        int percent = int.Parse(percentInput);
+        string letter;
+
+        if (percent >= 90) {
+            letter = "A";
+        } else if (percent >= 80) {
+            letter = "B";
+        } else if (percent >= 70) {
+            letter = "C";
+        } else if (percent >= 60) {
+            letter = "D";
+        } else {
+            letter = "F";
+        }
+        Console.WriteLine($"You got a(n) {letter}!");
+
+        if (percent >= 70) {
+            Console.WriteLine("This means you pass! Congrats!");
+        } else {
+            Console.WriteLine("This means you fail! Better luck next time!");
+        }
     }
 }
