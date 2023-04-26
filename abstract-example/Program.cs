9b8544c1364c80d2122a12ea8adcb1dd9bf1bf82
Braden Huttash <hut21013@byui.edu>
2023-04-26 12:29:30 -0600

    Book practice in class 4/26

---
+++ abstract-example/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Book mistborn = new Book("Mistborn", "Brandon Sanderson");
+
+mistborn.Display();
+
+Book hp1 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling");
+
+hp1.Display();
+
+Console.WriteLine(mistborn.isAvailable());
