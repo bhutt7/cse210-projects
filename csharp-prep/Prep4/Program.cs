c2636332d4e31e881d785cbf06067e985df696cc
Braden Huttash <hut21013@byui.edu>
2023-04-26 11:56:36 -0600

    finished prep 4 core

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -17,5 +17,21 @@ class Program
 
         } while (value != 0);
 
+        int sum = 0;
+        foreach (int num in numbers) {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float avg = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {avg}");
+
+        int highest = 0;
+        foreach (int num in numbers) {
+            if (num > highest) {
+                highest = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {highest}");
     }
 }
