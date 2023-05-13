1a0b2d4568eb92bff7a74b2dc21b0d2b27668f74
Braden Huttash <hut21013@byui.edu>
2023-05-13 17:44:30 -0600

    minor change to comment

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-// Exceeding Expectations: When the user creates a new entry, the program generates a prompt then removes it from the prompt list. Once the prompt list is empty, if the user tries to create a new entry, it instead tells them they have answered all prompts for the day (session), and sends them back to the menu.
+// Exceeding Requirements: When the user creates a new entry, the program generates a prompt then removes it from the prompt list. Once the prompt list is empty, if the user tries to create a new entry, it instead tells them they have answered all prompts for the day (session), and sends them back to the menu.
 
 class Program
 {
