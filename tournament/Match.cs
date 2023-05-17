a76eb548a0ef317064ce0a1c07d64259b3a752d9
Braden Huttash <hut21013@byui.edu>
2023-05-17 12:30:57 -0600

    in class tournament program

---
+++ tournament/Match.cs
@@ -0,0 +1,10 @@
+public class Match
+{
+    private Team _team1;
+    private Team _team2;
+
+    public Match(Team t1, Team t2)
+    {
+        
+    }
+}
---
+++ tournament/Program.cs
@@ -0,0 +1,13 @@
+﻿Player neymar = new Player("Neymar Jr.", 10, "Forward");
+
+Player kobe = new Player("Kobe Bryant", 24);
+kobe.SetPosition("Shooting Guard");
+
+Team awesomeSauce = new Team("Awesome Sauce");
+awesomeSauce.AddPlayer(kobe);
+awesomeSauce.AddPlayer(neymar);
+
+awesomeSauce.AddLoss();
+awesomeSauce.AddWin();
+
+awesomeSauce.DisplayRoster();
