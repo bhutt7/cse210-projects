d7f437a055fb3bb6894a1071f9567bbc6b10eee7
Braden Huttash <hut21013@byui.edu>
2023-06-05 12:30:13 -0600

    Elevator ice challenge

---
+++ elevator-ice/Program.cs
@@ -0,0 +1,10 @@
+﻿Elevator normalOne = new Elevator();
+Elevator normalTwo = new Elevator();
+VipElevator vip = new VipElevator();
+Runner run = new Runner(normalOne, normalTwo, vip);
+
+Console.WriteLine("Select an elevator");
+Console.WriteLine("1. Elevator One");
+Console.WriteLine("2. Elevator Two");
+Console.WriteLine("3. VIP Elevator");
+Console.Write("")
