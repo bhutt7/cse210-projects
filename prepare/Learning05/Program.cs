fb8fa09eac5645193c6bcf5f0b6f82f991e9d0df
Braden Huttash <hut21013@byui.edu>
2023-06-14 17:18:15 -0600

    finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("blue", 12);
+        Rectangle rectangle = new Rectangle("red", 12, 10);
+        Circle circle = new Circle("green", 4);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);  
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
